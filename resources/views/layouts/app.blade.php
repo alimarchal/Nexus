86109231ba70f8830008048a43868bbd9b825ca8
=Danish Abbasi <mdanishabbasi321@gmail.com>
2025-01-20 17:28:51 +0500

    add branch-target

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
-
+        @stack('header')
         <!-- Styles -->
         @livewireStyles
     </head>
--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -16,6 +16,12 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link href="{{ route('branch-targets.index') }}" :active="request()->routeIs('branch-targets.index')">
+                        {{ __('Branch Targets') }}
+                    </x-nav-link>
+                </div>
+
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ms-6">
