78881748961af4fad6e27c9619761465eea81e58
Ayesha Ashraf <akh50381@gmail.com>
2025-02-27 09:35:30 +0500

    chnge icon

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -69,7 +69,7 @@
                             <div class="text-2xl font-bold leading-8">Reports</div>
                             <div class="mt-1 text-base font-extrabold text-black">Daily Position</div>
                         </div>
-                        <img src="https://mc.imste.com/icons-images/accounting.png" alt="Account" class="h-16 w-16">
+                        <img src="{{url('icons-images/reportss.png') }}" alt="Report" class="h-16 w-16">
                     </div>
                 </a>
                 <a href="{{route('reports.deposit-advances-reports-branch')}}" class="transform hover:scale-110 transition duration-300 shadow-xl rounded-lg col-span-12 sm:col-span-6 xl:col-span-4 intro-y bg-white block">
