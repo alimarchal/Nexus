05f69113a18dd0255faa6205d987298c6bc583ff
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-08-20 16:06:21 +0500

    feat: Implement EmployeeResource management with controller, requests, policy, factory, migration, and seeder

---
+++ database/seeders/EmployeeResourceSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class EmployeeResourceSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
