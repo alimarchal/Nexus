ca88098d06bb6997462119b55fae77e6e7f8fda0
Ali Raza Marchal <alirazamarchal@hotmail.com>
2025-12-11 15:29:01 +0500

    Refactor RolesAndPermissionsSeeder to improve permission formatting and re-enable user creation for branches

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -27,21 +27,21 @@ class RolesAndPermissionsSeeder extends Seeder
             'create users',
             'edit users',
             'delete users',
-            
+
             // Role & Permission Management
             'view roles',
-            'create roles', 
+            'create roles',
             'edit roles',
             'delete roles',
             'view permissions',
             'create permissions',
-            'edit permissions', 
+            'edit permissions',
             'delete permissions',
             'assign roles',
             'revoke roles',
             'assign permissions',
             'revoke permissions',
-            
+
             // Complaint Management
             'view complaints',
             'create complaints',
@@ -49,7 +49,7 @@ class RolesAndPermissionsSeeder extends Seeder
             'delete complaints',
             'assign complaints',
             'escalate complaints',
-            
+
             // Audit Management
             'view audits',
             'create audits',
@@ -57,40 +57,40 @@ class RolesAndPermissionsSeeder extends Seeder
             'delete audits',
             'conduct audits',
             'review audits',
-            
+
             // Branch Management
             'view branches',
             'create branches',
-            'edit branches', 
+            'edit branches',
             'delete branches',
-            
+
             // Region Management
             'view regions',
             'create regions',
             'edit regions',
             'delete regions',
-            
+
             // Division Management
             'view divisions',
             'create divisions',
             'edit divisions',
             'delete divisions',
-            
+
             // Category Management
             'view categories',
             'create categories',
             'edit categories',
             'delete categories',
-            
+
             // Report Management
             'view reports',
             'generate reports',
             'export reports',
-            
+
             // Dashboard Access
             'view dashboard',
             'view analytics',
-            
+
             // Settings Management
             'view settings',
             'edit settings',
@@ -111,30 +111,77 @@ class RolesAndPermissionsSeeder extends Seeder
         // Define role permissions
         $rolePermissions = [
             'branch' => [
-                'view users', 'view complaints', 'create complaints', 'edit complaints', 
-                'view dashboard', 'view reports'
+                'view users',
+                'view complaints',
+                'create complaints',
+                'edit complaints',
+                'view dashboard',
+                'view reports'
             ],
             'region' => [
-                'view users', 'edit users', 'view complaints', 'create complaints', 
-                'edit complaints', 'assign complaints', 'view audits', 'view branches',
-                'view dashboard', 'view reports', 'generate reports'
+                'view users',
+                'edit users',
+                'view complaints',
+                'create complaints',
+                'edit complaints',
+                'assign complaints',
+                'view audits',
+                'view branches',
+                'view dashboard',
+                'view reports',
+                'generate reports'
             ],
             'division' => [
-                'view users', 'edit users', 'create users', 'view complaints', 
-                'create complaints', 'edit complaints', 'assign complaints', 
-                'escalate complaints', 'view audits', 'create audits', 'edit audits',
-                'view branches', 'edit branches', 'view dashboard', 'view reports', 
-                'generate reports', 'export reports'
+                'view users',
+                'edit users',
+                'create users',
+                'view complaints',
+                'create complaints',
+                'edit complaints',
+                'assign complaints',
+                'escalate complaints',
+                'view audits',
+                'create audits',
+                'edit audits',
+                'view branches',
+                'edit branches',
+                'view dashboard',
+                'view reports',
+                'generate reports',
+                'export reports'
             ],
             'head-office' => [
-                'view users', 'create users', 'edit users', 'delete users',
-                'view roles', 'edit roles', 'assign roles', 'revoke roles',
-                'view complaints', 'create complaints', 'edit complaints', 'delete complaints',
-                'assign complaints', 'escalate complaints', 'view audits', 'create audits', 
-                'edit audits', 'delete audits', 'conduct audits', 'review audits',
-                'view branches', 'create branches', 'edit branches', 'delete branches',
-                'view dashboard', 'view analytics', 'view reports', 'generate reports', 
-                'export reports', 'view settings', 'edit settings'
+                'view users',
+                'create users',
+                'edit users',
+                'delete users',
+                'view roles',
+                'edit roles',
+                'assign roles',
+                'revoke roles',
+                'view complaints',
+                'create complaints',
+                'edit complaints',
+                'delete complaints',
+                'assign complaints',
+                'escalate complaints',
+                'view audits',
+                'create audits',
+                'edit audits',
+                'delete audits',
+                'conduct audits',
+                'review audits',
+                'view branches',
+                'create branches',
+                'edit branches',
+                'delete branches',
+                'view dashboard',
+                'view analytics',
+                'view reports',
+                'generate reports',
+                'export reports',
+                'view settings',
+                'edit settings'
             ],
             'super-admin' => Permission::all()->pluck('name')->toArray()
         ];
@@ -147,17 +194,17 @@ class RolesAndPermissionsSeeder extends Seeder
             }
         }
 
-        // foreach(Branch::all() as $branch) {
-        //     $new_user = User::create([
-        //         'branch_id' => $branch->id,
-        //         'name' => 'Branch ' . $branch->code . '-' . $branch->district_id . '-' . $branch->region_id,
-        //         'email' => 'manager' . $branch->code . '@' . 'bankajk.com',
-        //         'password' => Hash::make('password'),
-        //         'email_verified_at' => now(),
-        //     ]);
+        foreach (Branch::all() as $branch) {
+            $new_user = User::create([
+                'branch_id' => $branch->id,
+                'name' => 'Branch ' . $branch->code . '-' . $branch->district_id . '-' . $branch->region_id,
+                'email' => 'manager' . $branch->code . '@' . 'bankajk.com',
+                'password' => Hash::make('password@999'),
+                'email_verified_at' => now(),
+            ]);
 
-        //     $new_user->assignRole('branch');
-        // }
+            $new_user->assignRole('branch');
+        }
 
         // Create super admin
         $user = User::create([
