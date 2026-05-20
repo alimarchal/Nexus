02f51969e801adfb505991052d4dd76a35d27f82
Ali Raza Marchal <kh.marchal@gmail.com>
2026-05-20 10:35:23 +0500

    feat: update AKSIC rules report layout and enhance table structure

--- resources/views/reports/aksic-rules-report.blade.php
+++ resources/views/reports/aksic-rules-report.blade.php
@@ -20,8 +20,8 @@
         </div>
     </x-slot>
 
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-4 pb-16 print:p-0 print:m-0">
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg print:shadow-none">
+    <div class="max-w-8xl mx-auto sm:px-6 lg:px-8 mt-4 pb-16 print:p-0 print:m-0">
+        <div class="bg-white dark:bg-gray-800 shadow-xl sm:rounded-lg print:shadow-none">
             <style>
                 @media print {
                     @page {
@@ -111,76 +111,78 @@
                     </div>
                 </div>
 
-                <table class="w-full text-sm border-collapse border border-slate-400 text-left text-black dark:text-gray-300 print:text-black">
-                    <caption class="caption-top text-left p-2 font-bold">District-wise AKSIC Rules and Loan Position</caption>
-                    <thead class="text-black uppercase bg-gray-50 dark:bg-gray-700 print:bg-gray-200">
-                        <tr>
-                            <th class="px-2 py-2 border border-black text-center">#</th>
-                            <th class="px-2 py-2 border border-black text-left">District</th>
-                            <th class="px-2 py-2 border border-black text-right">Population %</th>
-                            <th class="px-2 py-2 border border-black text-right">Proposed</th>
-                            <th class="px-2 py-2 border border-black text-right">Male 48%</th>
-                            <th class="px-2 py-2 border border-black text-right">Female 48%</th>
-                            <th class="px-2 py-2 border border-black text-right">Disabled 2%</th>
-                            <th class="px-2 py-2 border border-black text-right">Transgender 2%</th>
-                            <th class="px-2 py-2 border border-black text-right">Male Loans</th>
-                            <th class="px-2 py-2 border border-black text-right">Female Loans</th>
-                            <th class="px-2 py-2 border border-black text-right">Disabled Male</th>
-                            <th class="px-2 py-2 border border-black text-right">Disabled Female</th>
-                            <th class="px-2 py-2 border border-black text-right">Transgender Loans</th>
-                            <th class="px-2 py-2 border border-black text-right">Loans Done</th>
-                            <th class="px-2 py-2 border border-black text-right">Remaining</th>
-                            <th class="px-2 py-2 border border-black text-right">Loan Amount</th>
-                            <th class="px-2 py-2 border border-black text-right">Interest</th>
-                            <th class="px-2 py-2 border border-black text-right">Total Payable</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($reportRows as $row)
-                            <tr class="{{ $loop->even ? 'bg-gray-50 dark:bg-gray-700 print:bg-gray-100' : '' }}">
-                                <td class="px-2 py-2 border border-black text-center">{{ $loop->iteration }}</td>
-                                <td class="px-2 py-2 border border-black text-left">{{ $row['district'] }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['population_percentage'], 2) }}%</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['proposed_beneficiaries']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['male_beneficiaries']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['female_beneficiaries']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['disabled_beneficiaries']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['transgender_beneficiaries']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['actual_male_loans']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['actual_female_loans']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['actual_disabled_male_loans']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['actual_disabled_female_loans']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['actual_transgender_loans']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['loans_done']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['remaining']) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['principal_amount'], 2) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['interest_amount'], 2) }}</td>
-                                <td class="px-2 py-2 border border-black text-right">{{ number_format($row['total_payable'], 2) }}</td>
+                <div class="overflow-x-auto rounded-lg border border-slate-400 print:overflow-visible print:border-0">
+                    <table class="min-w-[1900px] w-full text-xs border-collapse text-left text-black dark:text-gray-300 print:min-w-full print:text-black">
+                        <caption class="caption-top text-left p-2 text-sm font-bold">District-wise AKSIC Rules and Loan Position</caption>
+                        <thead class="text-black uppercase bg-gray-50 dark:bg-gray-700 print:bg-gray-200">
+                            <tr>
+                                <th class="px-2 py-2 border border-black text-center whitespace-nowrap">#</th>
+                                <th class="px-2 py-2 border border-black text-left whitespace-nowrap">District</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Population %</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Proposed</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Male 48%</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Female 48%</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Disabled 2%</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Transgender 2%</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Male Loans</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Female Loans</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Disabled Male</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Disabled Female</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Transgender Loans</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Loans Done</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Remaining</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Loan Amount</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Interest</th>
+                                <th class="px-2 py-2 border border-black text-right whitespace-nowrap">Total Payable</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                    <tfoot class="font-bold bg-gray-100 dark:bg-gray-700 print:bg-gray-200">
-                        <tr>
-                            <td colspan="2" class="px-2 py-2 border border-black text-right">Totals</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['population_percentage'], 2) }}%</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['proposed_beneficiaries']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['male_beneficiaries']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['female_beneficiaries']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['disabled_beneficiaries']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['transgender_beneficiaries']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['actual_male_loans']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['actual_female_loans']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['actual_disabled_male_loans']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['actual_disabled_female_loans']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['actual_transgender_loans']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['loans_done']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['remaining']) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['principal_amount'], 2) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['interest_amount'], 2) }}</td>
-                            <td class="px-2 py-2 border border-black text-right">{{ number_format($totals['total_payable'], 2) }}</td>
-                        </tr>
-                    </tfoot>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($reportRows as $row)
+                                <tr class="{{ $loop->even ? 'bg-gray-50 dark:bg-gray-700 print:bg-gray-100' : '' }}">
+                                    <td class="px-2 py-2 border border-black text-center whitespace-nowrap">{{ $loop->iteration }}</td>
+                                    <td class="px-2 py-2 border border-black text-left whitespace-nowrap">{{ $row['district'] }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['population_percentage'], 2) }}%</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['proposed_beneficiaries']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['male_beneficiaries']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['female_beneficiaries']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['disabled_beneficiaries']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['transgender_beneficiaries']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['actual_male_loans']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['actual_female_loans']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['actual_disabled_male_loans']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['actual_disabled_female_loans']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['actual_transgender_loans']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['loans_done']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['remaining']) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['principal_amount'], 2) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['interest_amount'], 2) }}</td>
+                                    <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($row['total_payable'], 2) }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                        <tfoot class="font-bold bg-gray-100 dark:bg-gray-700 print:bg-gray-200">
+                            <tr>
+                                <td colspan="2" class="px-2 py-2 border border-black text-right whitespace-nowrap">Totals</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['population_percentage'], 2) }}%</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['proposed_beneficiaries']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['male_beneficiaries']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['female_beneficiaries']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['disabled_beneficiaries']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['transgender_beneficiaries']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['actual_male_loans']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['actual_female_loans']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['actual_disabled_male_loans']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['actual_disabled_female_loans']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['actual_transgender_loans']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['loans_done']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['remaining']) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['principal_amount'], 2) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['interest_amount'], 2) }}</td>
+                                <td class="px-2 py-2 border border-black text-right whitespace-nowrap">{{ number_format($totals['total_payable'], 2) }}</td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            maxWidth: {
+                '8xl': '96rem',
+            },
         },
     },
 
